dd7fae7aab6d411cab65cf6d35936dc696909b80
Bob Hitching <bob@hitching.net>
2025-01-16 03:44:46 +0000

    3 cols

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,97 +2,109 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("💬 Turn The Corner - GP Match")
+st.title("💬 Book an appointment with the best local GP")
 
-with st.chat_message("assistant"):
-    st.write("Hi! I’m Dr. Tamsin Franklin. TTC is a team of 35 GPs, who together bring an unrivalled breadth of experience for your healthcare needs and goals.")
-    st.write("Please answer some anonymous questions to help identify which of our GPs are the best match for your immediate or long term needs.")
+st.write("We are a team of 35 amazing GPs, who together bring an unrivalled breadth and depth of experience.")
+st.write("Chat privately with our AI to work out which of our GPs are the best match for your immediate or long term healthcare needs and goals.")
 
-if "ai_response" not in st.session_state: 
-    st.session_state.ai_response = None
+if "answers" not in st.session_state: 
+    st.session_state.answers = []
 
 client = OpenAI(api_key=st.secrets["openai_key"])
 def submit_enquiry():
-    prompt = f"You are the receptionist at a doctors clinic. Respond to the following patient enquiry with 3 answers, each from a fictional TV doctor giving an in-character response to the enquiry. For each doctor's response, include a statement of which location(s) from the enquiry the doctor works at, and when the doctor is available for an appointment. Here's the enquiry: {enquiry}."
+    prompt = f"You are the receptionist at Turn The Corner doctors. Respond to the following patient enquiry with 3 answers, each from a fictional TV doctor or Dr. Tamsin Franklin of Turn The Corner, each giving an in-character response to the enquiry. For each response, include a statement of which location(s) from the enquiry the doctor works at, and when the doctor is available for an appointment. Separate each response with the string <hr>. Here's the enquiry: {enquiry}."
 
-    st.session_state.ai_response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
+    ai_response = client.chat.completions.create(
+        model="gpt-4o-mini",
         messages=[
             {"role": "user", "content": prompt}
-        ],
-        stream=True,
+        ]
     )
+    response_dict = ai_response.model_dump()
+    message_content = response_dict["choices"][0]["message"]["content"]
 
-matching_option = st.selectbox(
-    'Firstly, do you want an appointment as soon as possible, or to find the best matched GP?',
-     ['', 'I\'m okay with any GP', 'I\'m looking for the best matched GP']
+    st.session_state.answers = message_content.split('<hr>')
+
+new_existing_option = st.selectbox(
+    'Firstly, have you been seen at Turn The Corner before?',
+     ['Existing patient', 'New patient']
 )
 
-if matching_option:
+if new_existing_option:
 
-    who_option = None
-    who_self_age = None
-    who_partner_age = None
+    locations = st.multiselect(
+        'Great! Which Turn The Corner locations can you get to?',
+        ['Northcote', 'Brunswick', 'Fairfield']
+    )
 
-    if 'best matched' in matching_option:
-        who_option = st.selectbox(
-            'Who are you choosing a GP for?',
-            ['', 'Myself', 'My family']
-        )
-
-        if who_option:
-            who_self_age = st.selectbox(
-                'What is your age?',
-                ['', '18 to 25', '26 to 35', '36 to 45', '46 to 55', '56 to 65', '66 to 75', 'Over 75']
-            )
-
-        if who_option == 'My family':
-            who_partner_age = st.selectbox(
-                'Do you have a partner? What is their age?',
-                ['', 'No', '18 to 25', '26 to 35', '36 to 45', '46 to 55', '56 to 65', '66 to 75', 'Over 75']
-            )
-
-    gender_pref = None
-    if ('any GP' in matching_option) or (who_self_age and (who_option == 'Myself' or who_partner_age)):
+    if locations:
         gender_pref = st.selectbox(
-            'Do you have a preference for a male or female GP?',
-            ['', 'No preference', 'Male', 'Female']
+            'And do you have a preference for a male or female GP?',
+            ['No preference', 'Male', 'Female']
         )
 
-        locations = []
         if gender_pref:
-            locations = st.multiselect(
-                'Which TTC locations can you get to?',
-                ['Northcote', 'Brunswick', 'Fairfield']
-            )
 
-            goals = None
-            if locations:
-                goals = st.text_area('What are your health issues and goals?')
+            goals = st.text_area('What are your health issues and goals?')
 
-                if goals:
-                    who_str = 'we\'re' if who_option == 'My family' else 'I\'m'
+            if goals:
+                target = 'the best GP'
+                if gender_pref == 'Male':
+                    target = 'the best male GP'
+                elif gender_pref == 'Female':
+                    target = 'the best female GP'
 
+                location_str = ' or '.join(locations)
+                enquiry_value = f'Hi, I\'m looking for {target} in {location_str} with expertise in: {goals}.'        
+                enquiry = st.text_area('Finally, add anything else you want to share with us. Your name and contact details are not required at this stage. Click submit to find your best matched GPs and their available appointment times:', enquiry_value)
 
-                    gender_str = ''
-                    location_str = ' or '.join(locations)
-                    
-                    family_str = 'for my family' if who_option == 'My family' else ''
-                    
-                    criteria = []
-                    if 'any GP' in matching_option:
-                        criteria.append('availability as soon as possible')
+                submit_button = st.button('Submit', type="primary", on_click=submit_enquiry)
 
-                    criteria.append(f'expertise in: {goals}')
-                    
-                    criteria_str = ', and '.join(criteria)
+if st.session_state.answers:
+    st.markdown(
+        """
+        <style>
+        .stColumn {
+            padding: 10px;
+            background-color: #f9f9f9;
+            border: 1px solid #ddd;
+            border-radius: 5px;
+            height: 500px;
+            overflow: scroll;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+    
+    cols = st.columns(len(st.session_state.answers))
 
-                    enquiry_value = f'Hi, {who_str} looking for a {gender_str} GP in {location_str} {family_str} with {criteria_str}.'
+    # Display each answer in its respective column
+    for idx, (col, answer) in enumerate(zip(cols, st.session_state.answers)):
 
-                    enquiry = st.text_area('Finally, edit your enquiry and click submit to find your best matched GPs and their available appointments:', enquiry_value)
+        with col:
 
-                    submit_button = st.button('Submit', type="primary", on_click=submit_enquiry)
+            if 'Heidi Hillis' in answer:
+                answer = answer.replace('Heidi Hillis', '<a href="https://fortunaadmissions.com/team-member/heidi-hillis/" target="_blank">Heidi Hillis</a>')
+                st.html('<a href="https://fortunaadmissions.com/team-member/heidi-hillis/" target="_blank"><img src="https://poetsandquants.com/wp-content/uploads/sites/5/2015/11/Headshots-420x420.jpg" width="100%" /></a>')
 
-if st.session_state.ai_response:
-    with st.chat_message("assistant"):
-        st.write_stream(st.session_state.ai_response)
+            st.markdown(answer, unsafe_allow_html=True)
+
+            name = st.text_input('Name', key=f'name-{idx}')
+            email = st.text_input('Email', key=f'email-{idx}')
+            enquiry_button = st.button('Submit', key=f'submit-{idx}', type="primary")
+
+            if 'Heidi Hillis' in answer:
+                st.html('<hr>')
+                st.html('<b>Recent reviews</b>')
+                st.html('<em>"Excellent Advisor - Stanford GSB Admit (Class Of 2027)"</em>')
+                st.html('⭐⭐⭐⭐⭐ <small><a href="https://poetsandquants.com/consultant/heidi-hillis/" target="_blank">3 weeks ago</a></small>')
+                st.html('<em>"Best Coach I Could Have Asked For! Best Coach! (Stanford / Columbia)"</em>')
+                st.html('⭐⭐⭐⭐⭐ <small><a href="https://poetsandquants.com/consultant/heidi-hillis/" target="_blank">1 month ago</a></small>')
+                st.html('<a href="https://poetsandquants.com/consultant/heidi-hillis/" target="_blank">More...</a>')
+
+                st.html('<hr>')
+                st.html('<b>Recent articles by Heidi</b>')
+                st.html('<a href="https://fortunaadmissions.com/how-to-create-a-career-vision-for-your-mba-application/" target="_blank">How to Create a Career Vision For Your MBA Application</a>')
+                st.html('<a href="https://fortunaadmissions.com/how-to-create-an-mba-career-vision-long-term-vs-short-term-goals/" target="_blank">MBA Goals: Long Term Vs. Short Term Career Vision</a>')
+                st.html('<a href="https://fortunaadmissions.com/author/heidi/" target="_blank">More...</a>')
